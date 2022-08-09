0dea21f90542e2d94b79bfd5f52b9e018fe81ec4
Thomas Van Breda <thomas100@live.com.au>
2022-08-10 00:00:38 +1000

    Fan control
    Starting to implement Fan control

---
+++ Assets/Scripts/FanControl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControl : MonoBehaviour
+{
+    public GameObject textPopUp;
+    public GameObject objectRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
